cd9de2d93aeb3e1bf8c7e82a65a83394d331f411
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-15 15:17:07 +0530

    [Rachit] Add. Calculate Employee Wage

--- EmployeWage.js
+++ EmployeWage.js
@@ -10,3 +10,24 @@ else
 {
     console.log("employee is present");
 }
+//UC2
+console.log("UC2");
+const WAGE_PER_HOUR=20;
+let emp_hours=0;
+let employeePresence= Math.floor((Math.random()*10)%3);
+switch(employeePresence)
+{
+    case 1:
+        console.log("PART TIME");
+        emp_hours=4;
+        break;
+    case 2:
+        console.log("FULL TIME");
+        emp_hours=8;
+        break;
+    default:
+        console.log("ABSENT");
+        break;
+}
+let employeeWage= WAGE_PER_HOUR*emp_hours;
+console.log(employeeWage);
