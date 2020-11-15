78556a22b4217e204b1ef3facc5a10e59df331ad
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-15 14:29:20 +0530

    Check for Employee Presence

---
+++ EmployeWage.js
@@ -0,0 +1,12 @@
+console.log("welcome to employee wage computation problem");
+//UC1
+let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
+const IS_ABSENT=0;
+if(checkForEmployeePresence==IS_ABSENT)
+{
+    console.log("employee is absent");
+}
+else
+{
+    console.log("employee is present");
+}
